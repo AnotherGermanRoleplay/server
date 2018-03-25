a467ee7244e448dc80985e8c2e583abe4cb55fa0
PolluX <12788936+RealPolluX@users.noreply.github.com>
2018-03-25 17:22:02 +0200

    added more alcohol

---
+++ resources/[esx]/esx_optionalneeds/esx_optionalneeds.sql
@@ -0,0 +1,9 @@
+USE `essentialmode`;
+
+INSERT INTO `items` (name, label) VALUES
+	('beer', 'Heineken Bier'),
+	('whiskey', 'Glas Whiskey'),
+	('vodka', 'Glas Vodka'),
+	('tequila', 'Tequila-Shot'),
+	('sekt', 'Flasche Sekt')
+;
--- resources/[esx]/esx_optionalneeds/server/main.lua
+++ resources/[esx]/esx_optionalneeds/server/main.lua
@@ -1,17 +1,48 @@
 ESX = nil
-
 TriggerEvent('esx:getSharedObject', function(obj)
 	ESX = obj
 end)
 
+-----------------------------------------------------------------------------------------------
+---------------------------------------[   ALCOHOL   ]-----------------------------------------
+-----------------------------------------------------------------------------------------------
+
 ESX.RegisterUsableItem('beer', function(source)
-
 	local xPlayer = ESX.GetPlayerFromId(source)
-
 	xPlayer.removeInventoryItem('beer', 1)
-
 	TriggerClientEvent('esx_status:add', source, 'drunk', 250000)
 	TriggerClientEvent('esx_optionalneeds:onDrink', source)
 	TriggerClientEvent('esx:showNotification', source, _U('used_beer'))
+end)
 
+ESX.RegisterUsableItem('whiskey', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeInventoryItem('whiskey', 1)
+	TriggerClientEvent('esx_status:add', source, 'drunk', 250000)
+	TriggerClientEvent('esx_optionalneeds:onDrink', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_whiskey'))
+end)
+
+ESX.RegisterUsableItem('vodka', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeInventoryItem('vodka', 1)
+	TriggerClientEvent('esx_status:add', source, 'drunk', 250000)
+	TriggerClientEvent('esx_optionalneeds:onDrink', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_vodka'))
+end)
+
+ESX.RegisterUsableItem('tequila', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeInventoryItem('tequila', 1)
+	TriggerClientEvent('esx_status:add', source, 'drunk', 250000)
+	TriggerClientEvent('esx_optionalneeds:onDrink', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_tequila'))
+end)
+
+ESX.RegisterUsableItem('sekt', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.removeInventoryItem('sekt', 1)
+	TriggerClientEvent('esx_status:add', source, 'drunk', 250000)
+	TriggerClientEvent('esx_optionalneeds:onDrink', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_sekt'))
 end)
