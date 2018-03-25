ae80ddac577b0c0a1bd325c91603a54b318a5229
PolluX <12788936+RealPolluX@users.noreply.github.com>
2018-03-25 14:48:00 +0200

    speedometer update & skins

--- resources/[rp]/SexySpeedometer/__resource.lua
+++ resources/[rp]/SexySpeedometer/__resource.lua
@@ -1,3 +1,23 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-client_script 'client.lua'
+client_scripts {
+	"client.lua",
+
+	-- add skins here
+	"skins/default.lua",
+	"skins/default_middle.lua",
+	"skins/id5.lua",
+	"skins/id6.lua",
+	"skins/id7.lua",
+}
+
+exports {
+	"getAvailableSkins",
+	"changeSkin",
+	"addSkin",
+	"toggleSpeedo",
+	"getCurrentSkin",
+	"addSkin",
+	"toggleFuelGauge",
+	"DoesSkinExist",
+}
