a0e093549053843b78dfa6c07ba448726314c25b
WR_Philipp <vagiwagner@gmail.com>
2018-04-07 02:20:40 +0200

    Revert "errorlog spam fix"
    This reverts commit d9577932f8224950a84f6896666b37f72f106c3e.

--- resources/[esx]/esx_status/server/main.lua
+++ resources/[esx]/esx_status/server/main.lua
@@ -67,9 +67,8 @@ AddEventHandler('esx_status:update', function(status)
 	
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
-	if xPlayer ~= nil then
-    xPlayer.set('status', status)
-  end
+	
+	xPlayer.set('status', status)
 
 end)
 
