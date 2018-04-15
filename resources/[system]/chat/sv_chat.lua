82e345af2688b0884d1751ce440e38a25a20365c
PolluX <12788936+RealPolluX@users.noreply.github.com>
2018-04-15 20:38:49 +0200

    added leave & join notification

--- resources/[system]/chat/sv_chat.lua
+++ resources/[system]/chat/sv_chat.lua
@@ -39,14 +39,29 @@ end)
 --    TriggerClientEvent('chatMessage', -1, '', { 255, 255, 255 }, '^2* ' .. GetPlayerName(source) .. ' joined.')
 --end)
 
---AddEventHandler('esx:playerLoaded', function(xPlayer)
---    TriggerClientEvent('chatMessage', -1, '', { 255, 255, 255 }, '^2* ' .. GetPlayerName(xPlayer) .. ' ist beigetreten.')
---end)
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+    local name = GetPlayerName(xPlayer)
 
---AddEventHandler('playerDropped', function(reason)
---    TriggerClientEvent('chatMessage', -1, '', { 255, 255, 255 }, '^2* ' .. GetPlayerName(source) ..' hat das Spiel verlassen (' .. reason .. ')')
---end)
+    TriggerServerEvent('discord_bot:admin_log', name .. 'joined the server.')
+    --TriggerClientEvent('chatMessage', -1, '', { 255, 255, 255 }, '^2* ' .. GetPlayerName(xPlayer) .. ' ist beigetreten.')
+    drawNotification( "~g~<C>" .. name .. "</C> ~s~joined." )
+end)
+
+AddEventHandler('playerDropped', function(reason)
+    local name = GetPlayerName(source)
+
+    TriggerServerEvent('discord_bot:admin_log', name ..' left the game (' .. reason .. ')')
+    --TriggerClientEvent('chatMessage', -1, '', { 255, 255, 255 }, '^2* ' .. GetPlayerName(source) ..' hat das Spiel verlassen (' .. reason .. ')')
+    drawNotification( "~r~<C>" .. name .. "</C> ~s~left." )
+end)
 
 RegisterCommand('say', function(source, args, rawCommand)
     TriggerClientEvent('chatMessage', -1, (source == 0) and 'console' or GetPlayerName(source), { 255, 255, 255 }, rawCommand:sub(5))
 end)
+
+-- Used to show notifications on the screen.
+function drawNotification(text)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawNotification(false, false)
+end
