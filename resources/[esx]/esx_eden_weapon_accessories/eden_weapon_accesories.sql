cd67154111e6b6bd629bcd4a9f048298922ef022
RealPolluX <pollux@team-quantum.org>
2018-05-01 19:24:08 +0200

    weapon accessories

---
+++ resources/[esx]/esx_eden_weapon_accessories/eden_weapon_accesories.sql
@@ -0,0 +1,4 @@
+INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (2010, 'yusuf', 'Skin', -1, 0, 1);
+INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (209, 'grip', 'Griff', -1, 0, 1);
+INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (208, 'flashlight', 'Lampe', -1, 0, 1);
+INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (207, 'silencieux', 'Schalldämpfer', -1, 0, 1);
