3082554110442c64437e4fedfd2a8aeb905ed1b0
RealPolluX <pollux@team-quantum.org>
2018-05-03 13:54:25 +0200

    removed sv_chat join/leave messages

--- resources/[system]/chat/sv_chat.lua
+++ resources/[system]/chat/sv_chat.lua
@@ -39,29 +39,29 @@ end)
 --    TriggerClientEvent('chatMessage', -1, '', { 255, 255, 255 }, '^2* ' .. GetPlayerName(source) .. ' joined.')
 --end)
 
-AddEventHandler('esx:playerLoaded', function(xPlayer)
-    local name = GetPlayerName(xPlayer)
+-- AddEventHandler('esx:playerLoaded', function(xPlayer)
+    -- local name = GetPlayerName(xPlayer)
 
-    TriggerEvent('discord_bot:admin_log', name .. 'joined the server.')
+    -- TriggerEvent('discord_bot:admin_log', name .. 'joined the server.')
     --TriggerClientEvent('chatMessage', -1, '', { 255, 255, 255 }, '^2* ' .. GetPlayerName(xPlayer) .. ' ist beigetreten.')
-    drawNotification( "~g~<C>" .. name .. "</C> ~s~joined." )
-end)
+    -- drawNotification( "~g~<C>" .. name .. "</C> ~s~joined." )
+-- end)
 
-AddEventHandler('playerDropped', function(reason)
-    local name = GetPlayerName(source)
+-- AddEventHandler('playerDropped', function(reason)
+    -- local name = GetPlayerName(source)
 
-    TriggerEvent('discord_bot:admin_log', name ..' left the game (' .. reason .. ')')
+    -- TriggerEvent('discord_bot:admin_log', name ..' left the game (' .. reason .. ')')
     --TriggerClientEvent('chatMessage', -1, '', { 255, 255, 255 }, '^2* ' .. GetPlayerName(source) ..' hat das Spiel verlassen (' .. reason .. ')')
-    drawNotification( "~r~<C>" .. name .. "</C> ~s~left." )
-end)
+    -- drawNotification( "~r~<C>" .. name .. "</C> ~s~left." )
+-- end)
 
 RegisterCommand('say', function(source, args, rawCommand)
     TriggerClientEvent('chatMessage', -1, (source == 0) and 'console' or GetPlayerName(source), { 255, 255, 255 }, rawCommand:sub(5))
 end)
 
 -- Used to show notifications on the screen.
-function drawNotification(text)
-    SetNotificationTextEntry("STRING")
-    AddTextComponentString(text)
-    DrawNotification(false, false)
-end
+-- function drawNotification(text)
+    -- SetNotificationTextEntry("STRING")
+    -- AddTextComponentString(text)
+    -- DrawNotification(false, false)
+-- end
