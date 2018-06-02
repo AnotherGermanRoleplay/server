6114be7c3352053e2e648e0021ab8058049524a3
PolluX <12788936+RealPolluX@users.noreply.github.com>
2018-06-02 16:32:30 +0200

    moneywash adjustments (more spots, percentage per spot)

--- resources/[esx]/esx_moneywash/config.lua
+++ resources/[esx]/esx_moneywash/config.lua
@@ -1,13 +1,17 @@
 Config              = {}
 Config.Slice        = 1000
-Config.Percentage   = 30
+-- Config.Percentage   = 30
 Config.MarkerType   = 1
 Config.DrawDistance = 10.0
 Config.MarkerColor  = {r = 204, g = 204, b = 204}
 Config.MarkerSize   = {x = 6.0, y = 6.0, z = 6.0}
-Config.openHours	= 0
-Config.closeHours	= 6
+Config.openHours	= 1
+Config.closeHours	= 5
 
 Config.Zones = {
-	{x = -2286.2436523438, y = 352.84228515625, z = 174.60177612305,percent=30},
+	{ x = -2286.2436523438, y = 352.84228515625, z = 174.60177612305, percent = 5 }, -- v1 pos
+	{ x = -28.056449890137, y = -724.55035400391, z = 44.228530883789, percent = 5 }, -- maze bank tower geldautomat
+	{ x = -329.87243652344, y = -780.52502441406, z = 33.964488983154, percent = 10 }, -- job autohändler automat oben 1.
+	{ x = -331.74462890625, y = -736.16632080078, z = 43.605197906494, percent = 15 }, -- job autohändler automat oben 3.
+	{ x = -1075.1480712891, y = -468.974609375, z = 36.650802612305, percent = 30 }, -- hollywood food truck
 }
