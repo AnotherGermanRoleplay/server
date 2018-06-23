ac4137ca644870141f1e175999b00a1c014331d3
root <root@ns3038310.ip-164-132-200.eu>
2018-06-23 18:41:31 +0000

    start.sh PROD

---
+++ start_autorestart.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+while true; do
+	if pgrep -x "v2" > /dev/null
+
+	then
+
+    	echo "Server is running"
+	else
+    	echo "Server crashed or stopped"
+    	echo "Server is restarting"
+    	screen -S v2 bash /home/v2/server/run.sh +exec /home/v2/data/server.cfg
+    	echo "Server has been started"
+
+	fi
+
+	sleep 5
+done
