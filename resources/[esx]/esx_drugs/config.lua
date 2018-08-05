711082a70bf966b09a7b8cd63e85e494873b3677
PolluX <12788936+RealPolluX@users.noreply.github.com>
2018-08-05 20:55:30 +0200

    now, 1 cop is needed for meth

--- resources/[esx]/esx_drugs/config.lua
+++ resources/[esx]/esx_drugs/config.lua
@@ -4,10 +4,28 @@ Config.DrawDistance = 100.0
 Config.ZoneSize     = {x = 5.0, y = 5.0, z = 3.0}
 --Config.MarkerColor  = {r = 100, g = 204, b = 100}
 Config.RequiredCopsCoke = 2 -- 2 -- 1
-Config.RequiredCopsMeth = 0 -- 2 -- 2
+Config.RequiredCopsMeth = 1 -- 2 -- 2
 Config.RequiredCopsWeed = 0 -- 0 -- 0
 Config.RequiredCopsOpium = 2 -- 3 -- 3
 Config.Locale = 'de'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 -- Config.Zones = {
 	-- CokeFarm = 		 {x=2448.9228515625,  y=-1836.8076171875, z=51.953701019287},
